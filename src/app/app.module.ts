cb2b7c995c7f92b0126711c3dc54d3842a79e7bb
Gisele Gomes <gselegomes@gmail.com>
2023-03-29 21:31:10 -0300

    feat: adds login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { AutocadastroComponent } from './componets/autocadastro/autocadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AutocadastroComponent
   ],
   imports: [
     BrowserModule
