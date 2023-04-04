ec20e62190eefca8894e1281f34b0e3528a99fee
Gisele Gomes <gselegomes@gmail.com>
2023-04-03 22:46:58 -0300

    feat: add pedido-online component + routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component"
+import { AutocadastroComponent } from "./components/autocadastro/autocadastro.component";
+import { PedidoOnlineComponent } from "./components/pedido-online/pedido-online.component";
+
+const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'autocadastro', component: AutocadastroComponent},
+    {path: 'pedido-online', component: PedidoOnlineComponent}
+]
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
---
+++ src/app/components/pedido-online/pedido-online.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-online',
+  templateUrl: './pedido-online.component.html',
+  styleUrls: ['./pedido-online.component.css']
+})
+
+export class PedidoOnlineComponent {
+
+}
