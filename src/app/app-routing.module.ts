e98070a930fc1167dd6cf4579723a84e0db04562
Eduardo <edubelo93@gmail.com>
2023-04-04 21:01:42 -0300

    adicinado tela inicial-cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component"
 import { AutocadastroComponent } from "./components/autocadastro/autocadastro.component";
 import { PedidoOnlineComponent } from "./components/pedido-online/pedido-online.component";
+import { InicialClienteComponent } from "./components/inicial-cliente/inicial-cliente.component";
 
 const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'autocadastro', component: AutocadastroComponent},
-    {path: 'pedido-online', component: PedidoOnlineComponent}
+    {path: 'pedido-online', component: PedidoOnlineComponent},
+    {path: 'inicial-cliente', component: InicialClienteComponent}
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './components/login/login.component';
 import { AutocadastroComponent } from './components/autocadastro/autocadastro.component';
 import { PedidoOnlineComponent } from './components/pedido-online/pedido-online.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InicialClienteComponent } from './components/inicial-cliente/inicial-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AutocadastroComponent,
-    PedidoOnlineComponent
+    PedidoOnlineComponent,
+    InicialClienteComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
