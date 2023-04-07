e7b0268f7f52454a8b74bd6adf54810ae779d75d
Gisele Gomes <gselegomes@gmail.com>
2023-04-07 01:12:54 -0300

    feat: tela inicial do cliente: tornando a tabela de pedidos dinamica + ajustando layout

--- src/app/components/inicial-cliente/inicial-cliente.component.ts
+++ src/app/components/inicial-cliente/inicial-cliente.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class InicialClienteComponent {
 
+  pedidos = [
+    { num_pedido: '1', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '2', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '3', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '4', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '5', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '6', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '7', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '8', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '9', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"}
+
+  ];
 }
+
