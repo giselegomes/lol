53e8d09f3b2d641578778dfd8a3bba0898f69164
Eduardo <edubelo93@gmail.com>
2023-04-10 22:47:17 -0300

    pequeno ajuste tabela inicial-cliente

--- src/app/pages/inicial-cliente/inicial-cliente.component.ts
+++ src/app/pages/inicial-cliente/inicial-cliente.component.ts
@@ -8,15 +8,15 @@ import { Component } from '@angular/core';
 export class InicialClienteComponent {
 
   pedidos = [
-    { num_pedido: '1', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '2', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '3', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '4', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '5', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '6', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '7', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '8', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
-    { num_pedido: '9', dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"}
+    { num_pedido: '1', status_pedido: "Cancelado", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '2', status_pedido: "Aprovado", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '3', status_pedido: "Aguardando Pagamento", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '4', status_pedido: "Em Aberto", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '5', status_pedido: "Em Aberto", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '6', status_pedido: "Em Aberto",  dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '7', status_pedido: "Em Aberto", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '8', status_pedido: "Em Aberto", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"},
+    { num_pedido: '9', status_pedido: "Em Aberto", dt_recolhimento: "10/12/2022", dt_devolucao: '12/12/2022', valor: "22,00"}
   ];
 }
 
