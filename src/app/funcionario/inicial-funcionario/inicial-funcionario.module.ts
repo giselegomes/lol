592c83de103ca44bbceabd445af4d4091539938b
Gisele Gomes <gselegomes@gmail.com>
2023-04-13 13:13:08 -0300

    feat: organizando pastas e adicionando paginas de funcionario

---
+++ src/app/funcionario/inicial-funcionario/inicial-funcionario.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InicialFuncionarioComponent } from './inicial-funcionario/inicial-funcionario.component';
+
+
+
+@NgModule({
+  declarations: [
+    InicialFuncionarioComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InicialFuncionarioModule { }
