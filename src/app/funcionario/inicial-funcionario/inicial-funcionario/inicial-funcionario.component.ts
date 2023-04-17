5b319a25076313861c9d8648ad7d478495dd3798
Gisele Gomes <gselegomes@gmail.com>
2023-04-17 00:38:00 -0300

    ajuste css

--- src/app/funcionario/inicial-funcionario/inicial-funcionario/inicial-funcionario.component.ts
+++ src/app/funcionario/inicial-funcionario/inicial-funcionario/inicial-funcionario.component.ts
@@ -10,12 +10,12 @@ import { Component } from '@angular/core';
 export class InicialFuncionarioComponent {
 
   pedidos = [
-    { num_pedido: '444', status_pedido: "Em Aberto", dataHora: new Date(2023, 4, 18, 10, 30) },
-    { num_pedido: '555', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 16, 10, 30) },
-    { num_pedido: '666', status_pedido: "Em Aberto", dataHora: new Date(2023, 2, 11, 10, 30) },
-    { num_pedido: '777', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 16, 10, 30) },
-    { num_pedido: '888', status_pedido: "Em Aberto", dataHora: new Date(2023, 4, 16, 10, 30) },
-    { num_pedido: '999', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 18, 10, 30) }
+    { num_pedido: '444', status_pedido: "Em Aberto", dataHora: new Date(2023, 4, 18, 1, 30) },
+    { num_pedido: '555', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 16, 1, 20) },
+    { num_pedido: '666', status_pedido: "Em Aberto", dataHora: new Date(2023, 2, 11, 13, 30) },
+    { num_pedido: '777', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 16, 9, 30) },
+    { num_pedido: '888', status_pedido: "Em Aberto", dataHora: new Date(2023, 4, 16, 10, 50) },
+    { num_pedido: '999', status_pedido: "Em Aberto", dataHora: new Date(2023, 3, 18, 8, 30) }
   ];
 
   // pedidos em ordem crescente
