305550b35ff5641d55b0162f40117eb1cdbd7f76
Gisele Gomes <gselegomes@gmail.com>
2023-05-14 02:20:15 -0300

    feat: ajuste pdf clientes

--- src/app/funcionario/relatorio-clientes/relatorio-clientes/relatorio-clientes.component.ts
+++ src/app/funcionario/relatorio-clientes/relatorio-clientes/relatorio-clientes.component.ts
@@ -11,31 +11,26 @@ import autoTable from 'jspdf-autotable'
   export class RelatorioClientesComponent {
 
     clientes = [
-      { nome: 'João', idade: 25, CEP: 78507120, estado: 'Minas Gerais', cidade: 'Sabará', rua: 'Duque de Caxias', numero: 9437 },
-      { nome: 'Maria', idade: 30, CEP: 71014676, estado: 'Paraíba', cidade: 'João P.', rua: 'Castro Alves', numero: 6788 },
-      { nome: 'Pedro', idade: 40,  CEP: 63762156, estado: 'Distrito Federal', cidade: 'Brasília', rua: 'Goiás', numero: 1268 }
+      { nome: 'João', CPF: 25, email: "teste@gmail.com", CEP: 78507120, uf: 'Minas Gerais', cidade: 'Sabará', endereco: 'Duque de Caxias', n: 9437, telefone: "9999 9999" },
+      { nome: 'Maria', CPF: 30, email: "teste@gmail.com", CEP: 71014676, uf: 'Paraíba', cidade: 'João P.', endereco: 'Castro Alves', n: 6788, telefone: "9999 9999" },
+      { nome: 'Pedro', CPF: 40,  email: "teste@gmail.com", CEP: 63762156, uf: 'Distrito Federal', cidade: 'Brasília', endereco: 'Goiás', n: 1268, telefone: "9999 9999" }
     ];
   
     gerarPDF() {
       const doc = new jsPDF();
+      const head = [['Nome', 'CPF', 'E-mail', 'CEP', 'Endereco', 'Nº', 'Cidade', 'UF', 'Telefone']]
+
+      doc.setFontSize(18);
       doc.text('Relatório de Clientes', 10, 10);
-      doc.text('Nome', 10, 20);
-      doc.text('Idade', 40, 20);
-      doc.text('CEP', 60, 20);
-      doc.text('Estado', 100, 20);
-      doc.text('Cidade', 150, 20);
-      doc.text('Numero', 180, 20);
   
-      let linha = 30;
-      this.clientes.forEach(cliente => {
-        doc.text(cliente.nome, 10, linha);
-        doc.text(cliente.idade.toString(), 40, linha);
-        doc.text(cliente.CEP.toString(), 60, linha);
-        doc.text(cliente.estado.toString(), 100, linha);
-        doc.text(cliente.cidade.toString(), 150, linha);
-        doc.text(cliente.numero.toString(), 180, linha);
-        linha += 10;
-      });
+      const rows =
+      this.clientes.map(cliente => [
+        cliente.nome, cliente.CPF, cliente.email, cliente.CEP, cliente.endereco, cliente.n, cliente.cidade, cliente.uf, cliente.telefone])
+      
+      autoTable(doc, {
+        head: head,
+        body: rows
+      })
   
       doc.save('relatorio-clientes.pdf');
     }
