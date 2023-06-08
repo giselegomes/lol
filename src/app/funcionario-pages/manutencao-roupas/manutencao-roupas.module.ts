9e92c334e1119d82192e04081cda9248c1f93a26
Eduardo <edubelo93@gmail.com>
2023-06-07 23:20:39 -0300

    atualizado para usar localstorage

--- src/app/funcionario-pages/manutencao-roupas/manutencao-roupas.module.ts
+++ src/app/funcionario-pages/manutencao-roupas/manutencao-roupas.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ManutencaoRoupasComponent } from './manutencao-roupas/manutencao-roupas.component';
 import { ComponentsModule } from 'src/app/components/components.module';
-
+import { RoupaService } from 'src/app/roupa/services/roupa.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     ManutencaoRoupasComponent,
+    
   ],
   imports: [
     CommonModule,
     ComponentsModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
+  ],
+  providers: [
+    RoupaService,
   ]
 })
 export class ManutencaoRoupasModule { }
--- src/app/funcionario-pages/manutencao-roupas/manutencao-roupas/manutencao-roupas.component.ts
+++ src/app/funcionario-pages/manutencao-roupas/manutencao-roupas/manutencao-roupas.component.ts
@@ -1,123 +1,104 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { LoginComponent } from 'src/app/login/login/login.component';
+import { RoupaService } from 'src/app/roupa/services/roupa.service';
+import { Roupa } from 'src/app/shared/models/roupa.model';
+import { Router } from '@angular/router';
 
-// classe que representa as peças de roupa
-export class itemRoupa {
-  nome: string;
-  valor: number;
-  prazo: string;
-
-  constructor(n: string, v: number, p: string){
-    this.nome = n;
-    this.valor = v;
-    this.prazo = p;
-  }
-}
 
 @Component({
   selector: 'app-manutencao-roupas',
   templateUrl: './manutencao-roupas.component.html',
   styleUrls: ['./manutencao-roupas.component.css']
 })
+export class ManutencaoRoupasComponent implements OnInit {
 
-export class ManutencaoRoupasComponent {
+  roupas: Roupa[] = [];
 
-  // prazo = quantidade de dias
-  itens: itemRoupa[] = [
-    { nome: "Blusa", valor: 5.50, prazo: "1" },
-    { nome: "Calça", valor: 7.00, prazo: "1" },
-    { nome: "Calcinha", valor: 2.00, prazo: "1" },
-    { nome: "Camisa", valor: 7.00, prazo: "1" },
-    { nome: "Camiseta", valor: 3.00, prazo: "1" },
-    { nome: "Casaco", valor: 10.00, prazo: "2" },
-    { nome: "Cueca", valor: 2.00, prazo: "1" },
-    { nome: "Gravata", valor: 1.00, prazo: "1" },
-    { nome: "Jaqueta", valor: 10.00, prazo: "2" },
-    { nome: "Meia", valor: 1.00, prazo: "1" },
-    { nome: "Moletom", valor: 9.50, prazo: "2" },
-    { nome: "Pijama", valor: 12.00, prazo: "1" },
-    { nome: "Regata", valor: 3.00, prazo: "2" },
-    { nome: "Roupa de Banho", valor: 6.00, prazo: "1" },
-    { nome: "Saia", valor: 5.00, prazo: "1" },
-    { nome: "Shorts", valor: 5.00, prazo: "1" },
-    { nome: "Suéter", valor: 12.00, prazo: "3" },
-    { nome: "Terno", valor: 25.00, prazo: "3" },
-    { nome: "Vestido", valor: 10.00, prazo: "2" }
-  ];
-
-  
-  // Item selecionado pelo usuário
-  itemSelecionado: itemRoupa;
-
-  adicionarItem() {
-    
-    
-    let roupa = new itemRoupa (document.getElementById('novoItemNome').innerHTML, 
-                               +document.getElementById('novoItemValor').innerHTML,
-                               document.getElementById('novoItemPrazo').innerHTML);
-    this.itens.push(roupa);
-  }
-
-  //falta fazer 
-  removerItem() {
-
-  };
-
-
-  //falta fazer
-  editarItem() {
-    //remover readonly
-    document.getElementById('prazoEditar').removeAttribute('readonly');
-    document.getElementById('valorEditar').removeAttribute('readonly');
-    //receber valor novo
-
-    //salvar
-
-  }
-
-  salvarEdicaoItem() {
-    let editadoPrazo = document.getElementById('prazoEditar').innerHTML;
-    let editadoValor: number = +document.getElementById('valorEditar').innerHTML;
-
-
-    this.itemSelecionado.prazo = editadoPrazo;
-    this.itemSelecionado.valor = editadoValor;
-
-
-
-  }
-
-  submit() {
-    console.log("enviou formulário");
-  }
-
-  momentForm!: FormGroup;
+  constructor(private roupaService: RoupaService) { }
 
   ngOnInit(): void {
-    this.momentForm = new FormGroup({
-      id: new FormControl(''),
-      valor: new FormControl('', Validators.required),
-      prazo: new FormControl('', Validators.required),
-
-      nome: new FormControl('', Validators.required)
-    })
+    //this.criaListaRoupa2();
+    this.roupas = this.roupaService.listarRoupas();
   }
-
-  get nome() {
-    return this.momentForm.get('nome')!;
+  //tudo o de baixo ta no RoupaSevice
+  
+  /*
+  listarRoupas(): Roupa[] {
+    return this.roupaService.listarRoupas();
+  
   }
-
-  get valor() {
-    return this.momentForm.get('nome')!;
+  */
+  /*
+  listarRoupas(): Roupa[]{
+    const roupas = localStorage["chave_roupas"];
+    return roupas ? JSON.parse(roupas) : [] ;
   }
-
-  get prazo() {
-    return this.momentForm.get('nome')!;
+  */
+  /*
+  removerRoupa($event: any, roupa: Roupa) {
+    $event.preventDefault();
+    if(confirm(`Deseja realmente remover o(a) ${roupa.nome}?`)) {
+      this.roupaService.removerRoupa(roupa.id!);
+      this.roupas = this.listarRoupas();
+    }
   }
+  */
 
-  public novoItem: boolean = false;
-  public alterarItem: boolean = false;
+  /*
+  inserirRoupa(roupa: Roupa): void {
 
+    const roupas = this.listarRoupas();
 
+    roupa.id = new Date().getTime();
 
+    roupas.push(roupa);
+
+    localStorage["chave_teste"] = JSON.stringify(roupas);
+  }
+  
+  /*
+  criarListaRoupa(): void{
+    this.inserirRoupa(new Roupa (1, "Blusa", 5.5, 1));
+    this.inserirRoupa(new Roupa (2, "Calça", 7.00,1));
+    this.inserirRoupa(new Roupa (3, "Calcinha", 2, 1));
+    this.inserirRoupa(new Roupa (4, "Camisa", 7, 1));
+    this.inserirRoupa(new Roupa (5, "Camiseta", 7, 1));
+    this.inserirRoupa(new Roupa (6, "Casaco", 10, 2));
+    this.inserirRoupa(new Roupa (7, "Cueca", 2, 1));
+    this.inserirRoupa(new Roupa (8, "Gravata", 1, 1));
+    this.inserirRoupa(new Roupa (9, "Jaqueta", 10, 2));
+    this.inserirRoupa(new Roupa (10, "Meia", 1, 1));
+    this.inserirRoupa(new Roupa (11, "Moletom", 9.5, 2));
+    
+  
+  }
+  */
+ /*
+  criaListaRoupa2(): void{
+  
+    let listaRoupas: Roupa[] = [
+      { id: 1, nome: "Blusa", valor: 5.50, prazo:1 },
+      { id: 2, nome: "Calça", valor: 7.00, prazo: 1 },
+      { id: 3, nome: "Calcinha", valor: 2.00, prazo: 1 },
+      { id: 4, nome: "Camisa", valor: 7.00, prazo: 1 },
+      { id: 5, nome: "Camiseta", valor: 3.00, prazo: 1 },
+      { id: 6, nome: "Casaco", valor: 10.00, prazo: 2 },
+      { id: 7, nome: "Cueca", valor: 2.00, prazo: 1 },
+      { id: 8, nome: "Gravata", valor: 1.00, prazo: 1 },
+      { id: 9, nome: "Jaqueta", valor: 10.00, prazo: 2 },
+      { id: 10, nome: "Meia", valor: 1.00, prazo: 1 },
+      { id: 11, nome: "Moletom", valor: 9.50, prazo: 2 },
+      { id: 12, nome: "Pijama", valor: 12.00, prazo: 1 },
+      { id: 13, nome: "Regata", valor: 3.00, prazo: 2 },
+      { id: 14, nome: "Roupa de Banho", valor: 6.00, prazo: 1 },
+      { id: 15, nome: "Saia", valor: 5.00, prazo: 1 },
+      { id: 16, nome: "Shorts", valor: 5.00, prazo: 1 },
+      { id: 17, nome: "Suéter", valor: 12.00, prazo: 3 },
+      { id: 18, nome: "Terno", valor: 25.00, prazo: 3 },
+      { id: 19, nome: "Vestido", valor: 10.00, prazo: 2 }
+    ]
+    localStorage.setItem("chave_roupas", JSON.stringify(listaRoupas));
+  } 
+  */
 }
